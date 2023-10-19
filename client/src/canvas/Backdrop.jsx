523c8065da63bf5500abc8166a8c149e5007e01b
Delano Igbinoba <delanoigbinoba@gmail.com>
2023-10-18 23:54:03 -0400

    fixed lighting and server issues

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -7,27 +7,26 @@ const Backdrop = () => {
   const shadows = useRef();
   return (
     <AccumulativeShadows
-      position={[0, 0, -0.14]}
       ref={shadows}
-      temporal
+      position={[0, 0, -0.14]}
       frames={60}
       alphaTest={0.85}
-      scale={10}
+      scale={5}
       rotation={[Math.PI / 2, 0, 0]}
     >
       <RandomizedLight
         amount={4}
         radius={9}
-        intensity={0.55}
-        ambient={0.25}
+        intensity={2.55}
+        ambient={0.75}
         position={[5, 5, -10]}
       />
       <RandomizedLight
         amount={4}
-        radius={9}
+        radius={5}
         intensity={0.25}
         ambient={0.55}
-        position={[-5, 5, -9]}
+        position={[-5, 5, -7]}
       />
     </AccumulativeShadows>
   );
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json({limit: "50mb"}))
 
 app.use('/api/v1/dalle', dalleRoutes)
-;app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).json({message: "Hello from DALL.E"})
 
 })
